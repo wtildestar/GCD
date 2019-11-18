95a8373ad32e7328d97b2c23c3dd12a1b3b14025
wtildestar <31348327+wtildestar@users.noreply.github.com>
2019-11-18 23:36:35 +0500

    Barrier playground for example

---
+++ GrandCentralDispatch/barrier-example.playground/Contents.swift
@@ -0,0 +1,37 @@
+import UIKit
+import PlaygroundSupport
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+class SafeArray<Element> {
+    private var array = [Element]()
+    private let queue = DispatchQueue(label: "DispatchBarrier", attributes: .concurrent)
+    
+    public func append(element: Element) {
+        queue.async(flags: .barrier) {
+            self.array.append(element)
+        }
+    }
+    
+    public var elements: [Element] {
+        var result = [Element]()
+        queue.sync {
+            result = self.array
+        }
+        
+        return result
+    }
+}
+
+var safeArray = SafeArray<Int>()
+DispatchQueue.concurrentPerform(iterations: 10) { (index) in
+    safeArray.append(element: index)
+}
+print("safeArray: \(safeArray.elements)")
+
+var array = [Int]()
+// делает параллельные запросы // данный метод вызовет ошибку из-за race condition
+DispatchQueue.concurrentPerform(iterations: 10) { (index) in
+    array.append(index)
+}
+print("array: \(array)")
